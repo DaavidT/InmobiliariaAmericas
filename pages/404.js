2f5df4614ce945b788adc62273185cf0154b7ed4
InvDevTalavera <david.talavera@invirtual.mx>
2023-05-31 15:57:10 -0600

    Actualizando plantilla

--- pages/404.js
+++ pages/404.js
@@ -1,38 +1,23 @@
-import Head from "next/head";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { NextSeo } from "next-seo";
+import NotFound from "@layouts/404";
+import Base from "@layouts/Baseof";
+import { getRegularPage } from "@lib/contentParser";
 
-export default function Contact() {
+const notFound = ({ data }) => {
   return (
-    <div className="text-black bg-black">
-      <NextSeo
-        title="404: nine4"
-        description="404 page for all our missing pages"
-        canonical="https://nine4-3.vercel.app/404"
-        openGraph={{
-          url: "https://nine4-3.vercel.app/404",
-        }}
-      />
-      <Head>
-        <title>nine4</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <div className="flex flex-col justify-center mx-auto mt-52 text-center max-w-2x1">
-        <h1 className="text-3xl font-bold tracking-tight text-white md:text-5xl">
-          404 – Unavailable
-        </h1>
-        <br />
-        <a
-          className="w-64 p-1 mx-auto font-bold text-center text-white border border-gray-500 rounded-lg sm:p-4"
-          href="/"
-        >
-          Return Home
-        </a>
-      </div>
-      <div className="mt-64"></div>
-      <Footer />
-    </div>
+    <Base>
+      <NotFound data={data} />
+    </Base>
   );
-}
+};
+
+// get 404 page data
+export const getStaticProps = async () => {
+  const notFoundData = await getRegularPage("404");
+  return {
+    props: {
+      data: notFoundData,
+    },
+  };
+};
+
+export default notFound;
